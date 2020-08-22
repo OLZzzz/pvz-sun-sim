7e04d54b5b8dcbc3ad0dccdfdb254c91e2edee23
Daniel Chen <chenzheye_daniel@outlook.com>
2020-08-22 15:51:09 +0800

    Second Commit

--- main.go
+++ main.go
@@ -9,14 +9,17 @@ import (
 	"time"
 )
 
-// 出怪总数为9种、10种、11种的概率分布
-var totalNumProb = []float64{0.1895, 0.5211, 0.2895}
+type seed [19]int
+
+var checkZomGen = false // 检查出怪分布是否正确
+var totalRun int
+var allZom seed
 
 func main() {
 	rand.Seed(time.Now().Unix())
 
 	// 计算9990起手的破阵率，arg1是总模拟次数，arg2是目标冲关f数
-	doSim(10000, 100)
+	// doSim(10000, 10000)
 
 	// 计算对于不同阳光起手的破阵率（0到9990），arg1是总模拟次数，arg2是目标冲关f数
 	drawSim(1000, 100)
@@ -46,6 +49,14 @@ func doSim(repeat int, limit int) {
 	}
 	fmt.Printf("Failure: %d / %d (%f)\n", fail, repeat, float64(fail)/float64(repeat))
 	fmt.Printf("Success: %d / %d (%f)\n", succ, repeat, float64(succ)/float64(repeat))
+
+	// 显示出怪详细
+	if checkZomGen {
+		for i := range allZom {
+			fmt.Printf("%.6f ", float64(allZom[i])/float64(totalRun))
+		}
+	}
+
 }
 
 func drawSim(repeat int, limit int) {
@@ -81,61 +92,49 @@ func drawSim(repeat int, limit int) {
 func getSunChange() int {
 	seed := rollSeed()
 
+	// 记录此次出怪
+	if checkZomGen {
+		totalRun++
+		for i := range seed {
+			allZom[i] += seed[i]
+		}
+	}
+
 	// 以下为多元线性回归求得的阳光变化公式
-	return int(2695 - 608*seed[2] - 886*seed[5] - 580*seed[7] - 582*seed[11] - 536*seed[17] - 2785*seed[18])
+	return int(2466 - 403*seed[2] - 789*seed[5] - 442*seed[7] - 604*seed[11] - 491*seed[17] - 2478*seed[18])
 }
 
 // 随机决定出怪
-func rollSeed() [19]int {
-	var seed [19]int
-
-	// 决定出怪总数
-	totalNum := rollTotalNum()
+func rollSeed() seed {
+	var seed seed
 
 	// 决定选路障或读报
-	flag := roll(0.8) // 若为true，选择路障；若为false，选择读报
-	if flag {
-		seed[0] = 1
-	} else {
+	flag := rand.Intn(5)
+	if flag == 0 {
 		seed[3] = 1
+	} else {
+		seed[0] = 1
 	}
 
 	// 通过对僵尸列表shuffle，随机选出剩下僵尸
+	// 路障 撑杆 铁桶 读报 铁门 橄榄 舞王 潜水 冰车 海豚 小丑 气球 矿工 跳跳 蹦极 扶梯 投篮 白眼 红眼 旗帜 僵王
+	// 其中，旗帜/僵王即使被选中，最终也会被忽略
 	zombies := make([]int, 0)
-	for i := 0; i < 19; i++ {
-		if flag && i == 0 {
+	for i := 0; i < 21; i++ {
+		if flag == 0 && i == 3 {
 			continue
 		}
-		if !flag && i == 3 {
+		if flag != 0 && i == 0 {
 			continue
 		}
 		zombies = append(zombies, i)
 	}
 	rand.Shuffle(len(zombies), func(i, j int) { zombies[i], zombies[j] = zombies[j], zombies[i] })
-	for i := 0; i < totalNum-1; i++ {
+	for i := 0; i < 9; i++ {
+		if zombies[i] >= 19 {
+			continue
+		}
 		seed[zombies[i]] = 1
 	}
 	return seed
-}
-
-// 随机决定出怪总数
-func rollTotalNum() int {
-	flag := roll(totalNumProb[0])
-	if flag {
-		return 8
-	}
-	flag = roll(totalNumProb[1] / (totalNumProb[1] + totalNumProb[2]))
-	if flag {
-		return 9
-	}
-	return 10
-}
-
-// 对于概率为prob的事件，随机决定此事件是否发生
-func roll(prob float64) bool {
-	f := 1 - rand.Float64() // 0.0 < f <= 1.0
-	if prob >= f {
-		return true
-	}
-	return false
 }
